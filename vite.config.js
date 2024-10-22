9447a27e07d944c8d2f4727bd604e754a737ee85
VertexApe <pete@vertexape.io>
2024-10-22 10:48:27 +0100

    config for material-ui and styled components

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@mui/styled-engine': '@mui/styled-engine-sc'
+    }
+  },
   build: {
     outDir: 'dist', // Ensure this matches your desired output folder
     assetsDir: 'assets', // This should also be where your assets (like SVGs) are placed
